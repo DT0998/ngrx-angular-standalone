878a368eae1fa6daa14683a37b5bbc86dbf791a8
DT0998 <duongkytrieuvn@gmail.com>
2023-09-28 15:13:57 +0700

    feat: add smth

---
+++ src/app/store/counter.reducer.ts
@@ -0,0 +1,12 @@
+import { createReducer, on } from '@ngrx/store';
+import { increment } from './counter.actions';
+
+const initialState = 0;
+export const counterReducer = createReducer(
+  initialState,
+  on(increment, (state,action) => state + action.value)
+);
+// other way write reducer
+// export function countReducer(state = initialState) {
+//   return state;
+// }
